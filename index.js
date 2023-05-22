822afafa2519f9c59836c8b0d9b80cb51e2c656c
jahangir2k04 <jahangir2k04gmail.com>
2023-05-22 20:26:22 +0600

    gallery photos data added in mongodb server and send to the client side

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const gallery = client.db('chadmamatoysDB').collection('gallery');
+
+    app.get('/gallery', async(req, res) => {
+      const result = await gallery.find().toArray();
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
